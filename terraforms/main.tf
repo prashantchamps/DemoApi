87c0739f816a6d90e5b4a1e55ae4fc9793f11ae4
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-16 01:52:41 +0530

    Update main.tf

--- terraforms/main.tf
+++ terraforms/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 resource "azurerm_app_service_plan" "main" {
-  name                = "api-appserviceplan"
+  name                = "api-demo-service-plan"
   location            = var.location
   resource_group_name = var.demo_api_rg
   kind                = "Linux"
@@ -14,7 +14,7 @@ resource "azurerm_app_service_plan" "main" {
   }
 }
 resource "azurerm_linux_web_app" "main" {
-  name                = "example"
+  name                = "api-demo-app"
   resource_group_name = var.demo_api_rg
   location            = var.location
   service_plan_id     = azurerm_service_plan.main.id
