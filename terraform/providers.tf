17105816f0ebd0f1f959a52d869986858e509f30
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-16 03:31:52 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,15 +1,19 @@
-provider "azurerm" {
-  skip_provider_registration = "true"
-  features {}
-}
-
 terraform {
+  required_version = ">= 1.3"
   backend "azurerm" {
     resource_group_name  = "main-infra"
     storage_account_name = "mydemotfstate"
     container_name       = "apitfstate"
     key                  = "actions.tfstate"
   }
+  required_providers {
+    azurerm = {
+      version = "~>3.2"
+      source  = "hashicorp/azurerm"
+    }
+  }
 }
 
-data "azurerm_client_config" "current" {}
+provider "azurerm" { 
+  features {}  
+}
