4cacd9fa8102a8ff116e6d6dba9672dc8fe1555b
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-16 01:36:39 +0530

    Update main.tf

--- terraforms/main.tf
+++ terraforms/main.tf
@@ -2,17 +2,21 @@ resource "azurerm_resource_group" "main" {
   name     = var.demo_api_rg
   location = var.location
 }
-
-resource "azurerm_storage_account" "main" {
-  name                     = "${var.app_name}appsa"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_app_service_plan" "main" {
+  name                = "api-appserviceplan"
+  location            = var.location
+  resource_group_name = var.demo_api_rg
+  kind                = "Linux"
+  reserved            = true
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
 }
-
-resource "azurerm_storage_container" "main" {
-  name                  = "content"
-  storage_account_name  = azurerm_storage_account.main.name
-  container_access_type = "private"
+resource "azurerm_linux_web_app" "main" {
+  name                = "example"
+  resource_group_name = var.demo_api_rg
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.main.id
+  site_config {}
 }
