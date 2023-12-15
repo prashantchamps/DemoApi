d2cfce41a8d99a17bf59df1818f83346c773abc9
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-16 00:53:04 +0530

    Update providers.tf

--- terraforms/providers.tf
+++ terraforms/providers.tf
@@ -1,1 +1,19 @@
-p
+terraform {
+  required_version = ">= 1.3"
+  backend "azurerm" {
+    resource_group_name  = "kopicloud-tfstate-rg"
+    storage_account_name = "kopicloudiactest"
+    container_name       = "core-tfstate"
+    key                  = "actions.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      version = "~>3.2"
+      source  = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" { 
+  features {}  
+}
