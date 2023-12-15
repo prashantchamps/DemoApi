af3fb56d3dfe4d2e8ee2c329717b980862fe162a
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-16 01:05:56 +0530

    Update providers.tf

--- terraforms/providers.tf
+++ terraforms/providers.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">= 1.3"
   backend "azurerm" {
-    resource_group_name  = "${var.tf_storage_rg_name}"
-    storage_account_name = "${var.tf_storage_name}"
-    container_name       = "${var.tf_storage_container_name}"
+    resource_group_name  = var.tf_storage_rg_name
+    storage_account_name = var.tf_storage_name
+    container_name       = var.tf_storage_container_name
     key                  = "actions.tfstate"
   }
   required_providers {
