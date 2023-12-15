bd6e09802637a884d0bf4b0b4ddb960f96427b3d
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-16 00:52:22 +0530

    Update variables.tf

--- terraforms/variables.tf
+++ terraforms/variables.tf
@@ -1,1 +1,10 @@
-
+variable "tf_storage_name" {
+  type        = string
+  description = "name of the storage account"
+  default     = "mydemotfstate"
+}
+variable "tf_storage_rg_name" {
+  type        = string
+  description = "name of the storage account resource group"
+  default     = "main-infra"
+}
