8a6b064a19f784b047ae5f81ebe0e4de11eb175c
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-16 02:45:24 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,28 @@
+variable "tf_storage_name" {
+  type        = string
+  description = "name of the storage account"
+  default     = "mydemotfstate"
+}
 
+variable "tf_storage_rg_name" {
+  type        = string
+  description = "name of the storage account resource group"
+  default     = "main-infra"
+}
+
+variable "tf_storage_container_name" {
+  type        = string
+  description = "name of the storage account resource group"
+  default     = "apitfstate"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of Resources"
+  default     = "eastus"
+}
+variable "demo_api_rg" {
+  type        = string
+  description = "name of api rg"
+  default     = "demo-application"
+}
