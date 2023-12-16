07ceeabde03e601c1a1ba93bee0ffe02db06f659
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-16 15:34:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,11 +20,18 @@ resource "azurerm_linux_web_app" "main" {
   resource_group_name = var.demo_api_rg
   location            = var.location
   service_plan_id     = azurerm_service_plan.main.id
+  https_only          = true
 
   depends_on = [
     azurerm_resource_group.main,
     azurerm_service_plan.main
   ]
 
-  site_config {}
+  site_config {
+    minimum_tls_version = "1.2"
+    application_stack {
+      java_server  = "JAVA"
+      java_version = "17"
+    }
+  }
 }
