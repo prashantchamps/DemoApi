71762cc8568f9774d5206a47a37102696374da15
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-16 16:00:35 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,9 +30,7 @@ resource "azurerm_linux_web_app" "main" {
   site_config {
     minimum_tls_version = "1.2"
     application_stack {
-      java_version = "jre17"
-      java_server  = "JAVA"
-      java_server_version = "17"
+      java_version = "JAVA|17-java17"
     }
   }
 }
