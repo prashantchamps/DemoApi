2277eaa1592d3227c501966adebd3d7da9e58111
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-19 00:23:19 +0530

    Update itineraryController.java

--- src/main/java/com/example/demo/itinerary/itineraryController.java
+++ src/main/java/com/example/demo/itinerary/itineraryController.java
@@ -1,13 +1,23 @@
 package com.example.demo.itinerary;
 
+import com.example.demo.Repo.EmployeeRepository;
+import com.example.demo.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class itineraryController {
-    @GetMapping("/itineraries")
-    private String getAllItineraries()
-    {
-        return "Testing Application";
+    @Autowired
+    EmployeeRepository repo;
+
+    @GetMapping("/employees")
+    public Iterable<Employee> findAllEmployees() {
+        return this.repo.findAll();
     }
+
+    /*@PostMapping("/employees")
+    public Employee addOneEmployee(@RequestBody Employee employee) {
+        return this.employeeRepository.save(employee);
+    }*/
 }
