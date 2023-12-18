49249e70294406c16c1c65c22f2f9a53f4be1c78
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-19 00:23:56 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.stream.Stream;
 
 @SpringBootApplication
-
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
+
