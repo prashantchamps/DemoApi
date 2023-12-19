842af5f950462905a4ac9538db8ff2386900e0ea
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-19 11:01:58 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,8 @@
-variable location {
+variable "location" {
   type        = string
   description = "Location of Resources"
 }
-variable demo_api_rg {
+variable "demo_api_rg" {
   type        = string
   description = "name of api rg"
 }
