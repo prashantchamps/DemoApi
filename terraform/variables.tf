e94874a35c373028984c0bd3fa48280c1b345a08
prashantchamps <42674656+prashantchamps@users.noreply.github.com>
2023-12-19 11:00:23 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,8 @@
 variable location {
   type        = string
   description = "Location of Resources"
-  default     = "East US"
 }
 variable demo_api_rg {
   type        = string
   description = "name of api rg"
-  default     = "demo-application"
 }
